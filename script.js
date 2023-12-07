dad1909baab9fcde3a0a865ac1af478a910eb82b
fariis21 <153177465+fariis21@users.noreply.github.com>
2023-12-07 17:05:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-question. innerHTML = "Aaaaa, I loveee u moreee peliii";
+question. innerHTML = "Aaaaa, I loveee u moree";
 gif.src =
 "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
  });
